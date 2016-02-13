13104620b1c52887a1e4ea1f60035dcc249d695e
Matheus Cendrao <matheuscendrao@gmail.com>
2016-02-12 23:27:47 -0200

    Removido console.log

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,6 @@ $(function(){
   $(window).scroll(function(){
 
     var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
-    console.log(wintop);
 
     if ((wintop / (docheight - winheight)) > 0.80) {
       var $scope = angular.element($('[ng-app]')).scope();
